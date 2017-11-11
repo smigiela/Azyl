460d11d058fb934f8f961c893eefb8e836bf3b14
Daniel <daniel.smigiela@outlook.com>
2017-11-11 02:07:44 +0000

    Dodano nawigacje do szablonu

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,6 @@
 
 require('./bootstrap');
+require('./custom');
 
 window.Vue = require('vue');
 
@@ -9,4 +10,13 @@ Vue.use(Buefy);
 
 
 // Vue.component('example', require('./components/Example.vue'));
+// var app = new Vue{{
+// 	el: '#app',
+// 	data: {}
+// }};
 
+$(document).ready(function(){
+	$('div.dropdown').hover(function(e){
+		$(this).toggleClass('is-open');
+	});
+});
